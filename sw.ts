77ce225e5898e4bdfd2f0f55858f42a169521897
logandavies181 <logan.davies181@gmail.com>
2025-01-01 21:06:35 +1300

    cache the app!

--- sw.ts
+++ sw.ts
@@ -1,1 +1,34 @@
-self.addEventListener('install', () => undefined)
+/// <reference lib="webworker" />
+//  https://github.com/denoland/deno/issues/15975
+declare const self: ServiceWorkerGlobalScope
+
+const sv = "0.1.0"
+
+// stored html/js/css
+const webCache = `web-${sv}`
+
+// TODO: static check here
+const urlsToCache = [
+  "index.html",
+  "dist/favicon.svg",
+  "dist/main.js",
+  "dist/output.css",
+]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(webCache)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((request) => {
+      return request || fetch(event.request)
+    })
+  )
+})
